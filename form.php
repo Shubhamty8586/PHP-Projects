ef2811a3d67eacff6a32792614a469a25583b651
Shubham Kumar Tyagi <98882978+Shubhamty8586@users.noreply.github.com>
2023-09-19 10:35:36 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Form</title>
+</head>
+<body>
+    <form action="ac.php" method="get">
+     Name:   <input type="text" name="name" id="name"><br>
+     Email:   <input type="email" name="email" id="email"><br>
+     Phone:   <input type="number" name="number" id="number"><br>
+     Message:   <textarea name="message" id="message" cols="30" rows="10"></textarea>
+     <input type="submit" value="submit">
+    </form>
+</body>
+</html>
