c873c538e45e7490e7c8deee599ce41e4c677972
Shubham Kumar Tyagi <98882978+Shubhamty8586@users.noreply.github.com>
2023-09-21 10:17:55 +0530

    Add files via upload

---
+++ ac.php
@@ -0,0 +1,8 @@
+<?php
+$name = $_GET['name'];
+$email =$_GET['email'];
+$number =$_GET['number'];
+$textarea =$_GET['message'];
+echo "Your Name is " .$name .", email is " .$email ." Phone number is "
+ .$number .", and Message is " .$textarea;
+?>
